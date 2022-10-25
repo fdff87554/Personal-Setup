e885c5f5715e60d4cbb1e5fdba036a049c9363fe
Crazyfire Lee <github@crazyfirelee.tw>
2022-10-25 16:02:25 +0800

    Add git setups

---
+++ git/git.sh
@@ -0,0 +1,6 @@
+git config --global user.email "github@crazyfirelee.tw"
+git config --global user.name "Crazyfire Lee"
+git config --global color.ui true
+git config --global core.editor "vim"
+git config --global alias.co commit
+git config --global alias.lg "log --color --graph --all --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --"
