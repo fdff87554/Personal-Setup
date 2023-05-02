da5d7511eda5de1ef54d1d9b54ea40f1914630e6
Crazyfire Lee <github@crazyfirelee.tw>
2023-05-03 02:52:23 +0800

    Update git.sh for user.email

--- git/git.sh
+++ git/git.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-git config --global user.email "github@crazyfirelee.tw"
+git config --global user.email "me@crazyfirelee.tw"
 git config --global user.name "Crazyfire Lee"
 git config --global color.ui true
 git config --global core.editor "vim"
