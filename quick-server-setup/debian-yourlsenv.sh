620f505bd72390658aa2972619a813ba797b2564
Crazyfire Lee <me@crazyfirelee.tw>
2023-10-08 13:58:09 +0800

    Updates

--- quick-server-setup/debian-yourlsenv.sh
+++ quick-server-setup/debian-yourlsenv.sh
@@ -3,7 +3,7 @@
 # Export command from sbin to path in .bashrc
 echo "Exporting command from sbin to path in .bashrc..."
 echo "export PATH=$PATH:/usr/local/sbin:/usr/sbin:/sbin" >>~/.bashrc
-export PATH=$PATH:/usr/sbin
+export PATH="$PATH":/usr/sbin
 
 # Basic setup for a Debian server running YOURLS
 echo "Basic setup for a Debian server running YOURLS"
@@ -14,7 +14,7 @@ sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y
 
 # Install need packages, and install docker
 echo "Installing git, vim, curl, wget, tmux, ssh-server..."
-sudo apt install -y git vim curl wget tmux openssh-server snapd
+sudo apt install -y git vim curl wget tmux openssh-server
 
 # Setup ssh server
 echo "Setting up ssh server..."
@@ -28,10 +28,12 @@ sudo sed -i 's/#PubkeyAuthentication no/PubkeyAuthentication yes/g' /etc/ssh/ssh
 # - Add public key into authorized_keys
 curl -L https://raw.githubusercontent.com/fdff87554/Personal-Setup/main/ssh/id_rsa.pub -o ~/crazyfire_id_rsa.pub
 # - Create .ssh folder if not exist
-if [ ! -d ~/.ssh ]; then
-    mkdir ~/.ssh
-    touch ~/.ssh/authorized_keys
-fi
+# if [ ! -d ~/.ssh ]; then
+#     mkdir ~/.ssh
+#     touch ~/.ssh/authorized_keys
+# fi
+mkdir ~/.ssh
+touch ~/.ssh/authorized_keys
 cat ~/crazyfire_id_rsa.pub >>~/.ssh/authorized_keys
 rm ~/crazyfire_id_rsa.pub
 
@@ -61,11 +63,29 @@ sudo usermod -aG docker $USER
 newgrp docker
 rm get-docker.sh
 
-# Install Certbot
-echo "Installing certbot..."
-# - Install snapd
-sudo apt install snapd -y
-# - Install certbot
-sudo snap install --classic certbot
-# - Prepare the Certbot command
-sudo ln -s /snap/bin/certbot /usr/bin/certbot
+# Setup Static IP to 192.168.50.200
+# Change allow-hotplug ens18 to auto ens18
+# Change iface ens18 inet dhcp to iface ens18 inet static
+# Add address 192.168.50.200
+# Add netmask 255.255.255.0
+# Add gateway 192.168.50.254
+# Add dns-nameservers 192.168.51.53
+echo "Setting up static IP ..."
+# - Prepare Backup
+sudo cp /etc/network/interfaces /etc/network/interfaces.dhcp.bk
+# - Change interfaces file
+sudo sed -i 's/allow-hotplug ens18/auto ens18/g' /etc/network/interfaces
+sudo sed -i 's/iface ens18 inet dhcp/iface ens18 inet static/g' /etc/network/interfaces
+printf "\naddress 192.168.50.200" | sudo tee -a /etc/network/interfaces
+printf "\nnetmask 255.255.255.0" | sudo tee -a /etc/network/interfaces
+printf "\ngateway 192.168.50.254" | sudo tee -a /etc/network/interfaces
+printf "\ndns-nameservers 192.168.51.53" | sudo tee -a /etc/network/interfaces
+
+# # Install Certbot
+# echo "Installing certbot..."
+# # - Install snapd
+# sudo apt install snapd -y
+# # - Install certbot
+# sudo snap install --classic certbot
+# # - Prepare the Certbot command
+# sudo ln -s /snap/bin/certbot /usr/bin/certbot
