ed64e9cd1db72e1ae2f7e73779c304fda946f2b1
Crazyfire Lee <crazyfirelee@gmail.com>
2025-05-22 14:46:54 +0800

    Update git.sh for sub module comment

--- git/git.sh
+++ git/git.sh
@@ -5,3 +5,6 @@ git config --global color.ui true
 git config --global core.editor "vim"
 git config --global alias.co commit
 git config --global alias.lg "log --color --graph --all --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --"
+git config --global alias.supdate "submodule update --init --recursive"
+git config --global alias.supdate-remote "submodule update --remote --rebase"
+git config --global alias.sstatus "submodule status"
